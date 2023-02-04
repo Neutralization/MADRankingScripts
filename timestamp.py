6454fc31047109568ac10647c3369b953a5997aa
Neutralization <50076677+Neutralization@users.noreply.github.com>
2023-02-04 18:03:10 +0800

    分段进度条

---
+++ timestamp.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import http.cookiejar
+import json
+
+import requests
+
+
+def main():
+    stamp = json.dumps(json.load(open("stamp.json", "r", encoding="utf-8")))
+
+    session = requests.session()
+    jar = http.cookiejar.MozillaCookieJar("./cookies.txt")
+    jar.load(ignore_discard=True, ignore_expires=True)
+    session.cookies = jar
+
+    params = (
+        ("mid", "398300398"),
+        ("ps", "30"),
+        ("tid", "0"),
+        ("pn", "1"),
+        ("keyword", "MAD"),
+        ("order", "pubdate"),
+        ("jsonp", "jsonp"),
+    )
+    response = session.get("https://api.bilibili.com/x/space/arc/search", params=params)
+    result = json.loads(response.content)
+    bvid = result["data"]["list"]["vlist"][0]["bvid"]
+    print(bvid)
+
+    params = (("bvid", bvid),)
+    response = session.get(
+        "https://api.bilibili.com/x/web-interface/view", params=params
+    )
+    result = json.loads(response.content)
+    aid = result["data"]["aid"]
+    cid = result["data"]["pages"][0]["cid"]
+
+    stampdata = {
+        "aid": aid,
+        "cid": cid,
+        "type": "2",
+        "cards": stamp,
+        "permanent": "false",
+        "csrf": session.cookies._cookies[".bilibili.com"]["/"]["bili_jct"].value,
+    }
+    response = session.post(
+        "https://member.bilibili.com/x/web/card/submit", data=stampdata
+    )
+    print(json.loads(response.content))
+
+
+if __name__ == "__main__":
+    main()
