1112a5957d72a00645a72fd241cc7e8fbff89375
Neutralization <50076677+Neutralization@users.noreply.github.com>
2023-07-12 00:24:10 +0800

    调整时间范围计算

--- topickup.py
+++ topickup.py
@@ -7,18 +7,26 @@ from math import floor
 
 import arrow
 
-BEFORE = floor(
-    (int(arrow.now("Asia/Shanghai").timestamp()) - 1428681600) / 3600 / 24 / 7
+Season = (int(arrow.now("Asia/Shanghai").format("M")) - 1) // 3 * 3 + 1  # 1 4 7 10
+EDay = arrow.get(
+    f'{arrow.now("Asia/Shanghai").format("YYYY")}-{Season}-1 00:00:00 +0800',
+    "YYYY-M-D HH:mm:ss Z",
 )
-AFTER = floor(
-    (
-        int(arrow.now("Asia/Shanghai").shift(months=-3).shift(weeks=1).timestamp())
-        - 1428681600
+SDay = EDay.shift(months=-3)
+isMon = int(EDay.format("d"))
+if isMon == 1:
+    ENum = floor((int(EDay.shift(days=-7).timestamp()) - 1428681600) / 3600 / 24 / 7)
+else:
+    ENum = floor(
+        (int(EDay.shift(days=1 - isMon).timestamp()) - 1428681600) / 3600 / 24 / 7
     )
-    / 3600
-    / 24
-    / 7
-)
+wasMon = int(SDay.format("d"))
+if wasMon == 1:
+    SNum = floor((int(SDay.timestamp()) - 1428681600) / 3600 / 24 / 7)
+else:
+    SNum = floor(
+        (int(SDay.shift(days=8 - wasMon).timestamp()) - 1428681600) / 3600 / 24 / 7
+    )
 
 
 def readjson(week):
@@ -31,14 +39,20 @@ def readjson(week):
     return pickup
 
 
-data = reduce(list.__add__, map(readjson, range(BEFORE, AFTER)))
+def main():
+    print(SNum, ENum)
+    data = reduce(list.__add__, map(readjson, range(SNum, ENum)))
 
-with open("./psdownload/download.txt", "w", encoding="utf-8") as f:
-    f.writelines([f"av{x['av']}\n" for x in data])
+    with open("./psdownload/download.txt", "w", encoding="utf-8") as f:
+        f.writelines([f"av{x['av']}\n" for x in data])
 
-json.dump(
-    sorted(data, key=lambda z: z["rank"]),
-    open("pickup.json", "w", encoding="utf-8"),
-    ensure_ascii=False,
-    indent=4,
-)
+    json.dump(
+        sorted(data, key=lambda z: z["rank"]),
+        open("pickup.json", "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
+
+
+if __name__ == "__main__":
+    main()
