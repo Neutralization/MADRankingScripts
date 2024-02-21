7fdcf91fd3c8d5281e3d710f369d78ccb06be191
Neutralization <50076677+Neutralization@users.noreply.github.com>
2024-02-21 19:59:16 +0800

    更好的信息输出

--- normalize.ps1
+++ normalize.ps1
@@ -4,64 +4,83 @@ param (
 $ProgressPreference = 'SilentlyContinue'
 $TruePath = Split-Path $MyInvocation.MyCommand.Path
 $DataFolder = "$($TruePath)/DATA"
-$DownloadFolder = "$($TruePath)/VIDEO"
+$DownloadFolder = "$($TruePath)/FOOTAGE/VIDEO"
+$FootageFolder = "$($TruePath)/FOOTAGE/No.$($RankNum)/VIDEO"
 
 if (Test-Path -Path 'C:/Windows/System32/nvcuvid.dll') { $Nvdia = $true } else { $Nvdia = $false }
 if ((WMIC CPU Get Name) -match 'Intel') { $Intel = $true } else { $Intel = $false }
-if (-Not (Test-Path -Path "$($DownloadFolder)/ORIGINAL")) { New-Item -Path "$($DownloadFolder)/ORIGINAL" -ItemType Directory }
-if (-Not (Test-Path -Path "$($DownloadFolder)/$($RankNum)")) { New-Item -Path "$($DownloadFolder)/$($RankNum)" -ItemType Directory }
+
 function Normailze {
     param (
         [parameter(position = 1)]$FileName,
         [parameter(position = 2)]$Offset,
         [parameter(position = 3)]$Length
     )
-    if (-Not (Test-Path "$($DownloadFolder)/ORIGINAL/$($FileName).mp4")) {
-        Write-Host "$($FileName).mp4 Not Exist!" -ForegroundColor Red
+    if ($LostVideos -contains $FileName) {
+        Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - $($FileName) 视频已失效，生成占位视频"
+        $FakeArg = -join @(
+            '-n -hide_banner -t 40 -f lavfi -i anullsrc -f lavfi '
+            '-i color=size=1280x720:duration=60:rate=60:color=AntiqueWhite '
+            "-vf drawtext=fontfile=MiSans-Medium.ttf:fontsize=147:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$($FileName)' "
+            "$($FootageFolder)/$($FileName).mp4"
+        )
+        Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -ArgumentList $FakeArg
         return $null
     }
     $Target = 'loudnorm=I=-23.0:LRA=+7.0:tp=-1.0'
     $Length = $Length + 5
-    $AudioArg = "-y -hide_banner -ss $($Offset) -t $($Length) -i $($DownloadFolder)/ORIGINAL/$($FileName).mp4 -af $($Target):print_format=json -f null -"
-    $AudioInfo = "$($DownloadFolder)/ORIGINAL/$($FileName).log"
+    $AudioArg = "-y -hide_banner -ss $($Offset) -t $($Length) -i $($DownloadFolder)/$($FileName).mp4 -af $($Target):print_format=json -f null -"
+    $AudioInfo = "$($DownloadFolder)/$($FileName).log"
+    Write-Host "$(Get-Date -Format 'MM/dd HH:mm:ss') - 分析 $($FileName) 音频数据" -ForegroundColor Green
     Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -RedirectStandardError $AudioInfo -ArgumentList $AudioArg
     $AudioData = Get-Content -Path $AudioInfo | Select-Object -Last 12 | ConvertFrom-Json
+    Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - $($AudioData)"
     $Source = "measured_I=$($AudioData.input_i):measured_LRA=$($AudioData.input_lra):measured_tp=$($AudioData.input_tp):measured_thresh=$($AudioData.input_thresh):offset=$($AudioData.target_offset)"
-    Write-Host "ffmpeg -af 'measured_I=$($AudioData.input_i):LRA=$($AudioData.input_lra):tp=$($AudioData.input_tp)' -> '$($Target)'" -ForegroundColor Blue
-    Write-Host "ffmpeg -ss $($Offset) -t $($Length) -i $($FileName).mp4" -ForegroundColor Green
+    Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - $($Source)"
     if ($Nvdia) {
         # Nvidia CUDA
-        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
-            + "-hwaccel_output_format cuda -c:v h264_cuvid "`
-            + "-i $($DownloadFolder)/ORIGINAL/$($FileName).mp4 "`
-            + "-vf scale='ceil((min(1,gt(iw,1280)+gt(ih,720))*(gte(a,1280/720)*1280+lt(a,1280/720)*((720*iw)/ih))+not(min(1,gt(iw,1280)+gt(ih,720)))*iw)/2)*2:ceil((min(1,gt(iw,1280)+gt(ih,720))*(lte(a,1280/720)*720+gt(a,1280/720)*((1280*ih)/iw))+not(min(1,gt(iw,1280)+gt(ih,720)))*ih)/2)*2' "`
-            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
-            + "-c:v h264_nvenc -b:v 10M -c:a aac -b:a 320k -r 30 $($DownloadFolder)/$($RankNum)/$($FileName).mp4"
+        Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - 使用 Nvidia CUDA 加速转码"
+        $VideoArg = -join @(
+            "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -hwaccel_output_format cuda -c:v h264_cuvid "
+            "-i $($DownloadFolder)/$($FileName).mp4 "
+            "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "
+            "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "
+            "-c:v h264_nvenc -b:v 10M -c:a aac -b:a 320k -r 60 $($FootageFolder)/$($FileName).mp4"
+        )
     } elseif ($Intel) {
         # Intel QSV
-        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
-            + "-init_hw_device qsv=hw -filter_hw_device hw "`
-            + "-i $($DownloadFolder)/ORIGINAL/$($FileName).mp4 "`
-            + "-vf hwupload=extra_hw_frames=64,format=qsv "`
-            + "-vf scale='ceil((min(1,gt(iw,1280)+gt(ih,720))*(gte(a,1280/720)*1280+lt(a,1280/720)*((720*iw)/ih))+not(min(1,gt(iw,1280)+gt(ih,720)))*iw)/2)*2:ceil((min(1,gt(iw,1280)+gt(ih,720))*(lte(a,1280/720)*720+gt(a,1280/720)*((1280*ih)/iw))+not(min(1,gt(iw,1280)+gt(ih,720)))*ih)/2)*2' "`
-            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
-            + "-c:v h264_qsv -b:v 10M -c:a aac -b:a 320k -r 30 $($DownloadFolder)/$($RankNum)/$($FileName).mp4"
+        Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - 使用 Intel QSV 加速转码"
+        $VideoArg = -join @(
+            "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -init_hw_device qsv=hw -filter_hw_device hw "
+            "-i $($DownloadFolder)/$($FileName).mp4 -vf hwupload=extra_hw_frames=64,format=qsv "
+            "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "
+            "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "
+            "-c:v h264_qsv -b:v 10M -c:a aac -b:a 320k -r 60 $($FootageFolder)/$($FileName).mp4"
+        )
     } else {
         # x264
-        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
-            + "-i $($DownloadFolder)/ORIGINAL/$($FileName).mp4 "`
-            + "-vf scale='ceil((min(1,gt(iw,1280)+gt(ih,720))*(gte(a,1280/720)*1280+lt(a,1280/720)*((720*iw)/ih))+not(min(1,gt(iw,1280)+gt(ih,720)))*iw)/2)*2:ceil((min(1,gt(iw,1280)+gt(ih,720))*(lte(a,1280/720)*720+gt(a,1280/720)*((1280*ih)/iw))+not(min(1,gt(iw,1280)+gt(ih,720)))*ih)/2)*2' "`
-            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
-            + "-c:v libx264 -b:v 10M -c:a aac -b:a 320k -r 30 $($DownloadFolder)/$($RankNum)/$($FileName).mp4"
+        Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - 使用 CPU x264 转码"
+        $VideoArg = -join @(
+            "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -i $($DownloadFolder)/$($FileName).mp4 "
+            "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "
+            "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "
+            "-c:v libx264 -b:v 10M -c:a aac -b:a 320k -r 60 $($FootageFolder)/$($FileName).mp4"
+        )
     }
+    Write-Host "$(Get-Date -Format 'MM/dd HH:mm:ss') - 截取视频并标准化音频" -ForegroundColor Green
     Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -ArgumentList $VideoArg
-    Write-Host "$($FileName) Finish!" -ForegroundColor White
+    Write-Host "$(Get-Date -Format 'MM/dd HH:mm:ss') - $($FileName) 操作完成`n" -ForegroundColor Green
 }
 
 function Main {
+    if (!(Test-Path "$($TruePath)/FOOTAGE/No.$($RankNum)/VIDEO")) {
+        New-Item -ItemType 'Directory' -Path "$($TruePath)/FOOTAGE/No.$($RankNum)/VIDEO"
+    }
     $RankVideos = @()
-    if ($RankNum -eq "pickup") {
-        $JsonData = Get-Content -Path "pickup.json"
+    $LocalVideos = @()
+    Get-ChildItem "$($FootageFolder)/*.mp4" | ForEach-Object { $LocalVideos += $_.BaseName }
+    if ($RankNum -eq 'pickup') {
+        $JsonData = Get-Content -Path 'pickup.json'
     } else {
         $JsonData = Get-Content -Path "$($DataFolder)/$($RankNum)期数据.json"
     }
@@ -76,13 +95,17 @@ function Main {
             $RankVideos += @{n = "av$($_.av)"; o = $_.offset; l = 40 }
         }
     }
-
     $RankVideos | ForEach-Object {
-        Normailze $_.n $_.o $_.l
+        if (($LocalVideos -notcontains $_.n) -or ((Get-Item "$($FootageFolder)/$($_.n).mp4").length -eq 0)) {
+            Normailze $_.n $_.o $_.l # -Debug
+        } else {
+            Write-Host "$(Get-Date -Format 'MM/dd HH:mm:ss') - $($_.n) 已存在，跳过处理" -ForegroundColor Green
+        }
+        
     }
 
     Add-Type -AssemblyName Microsoft.VisualBasic
-    Get-ChildItem "$($DownloadFolder)/ORIGINAL/*.log" | ForEach-Object {
+    Get-ChildItem "$($DownloadFolder)/*.log" | ForEach-Object {
         [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile(
             "$($_)", 'OnlyErrorDialogs', 'SendToRecycleBin')
     }
